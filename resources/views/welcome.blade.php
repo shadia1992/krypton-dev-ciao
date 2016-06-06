6263da75794f1ed08623869ff37d6e96747798c9
randsom <sandoz.romain@gmail.com>
2016-06-06 16:18:59 +0200

    My first change

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,7 +38,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Laravel 5</div>
+                <div class="title">KRYPTON TA MERE LOL</div>
             </div>
         </div>
     </body>
