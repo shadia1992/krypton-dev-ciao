9f91f67b50da169482ec729f5f5bc077ee595c09
randsom <sandoz.romain@gmail.com>
2016-06-06 16:35:41 +0200

    Second test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -39,6 +39,7 @@
         <div class="container">
             <div class="content">
                 <div class="title">KRYPTON TA MERE LOL</div>
+                <h2>Ceci est un test, mofo</h2>
             </div>
         </div>
     </body>
