cf8153429e4d466333c3f888177bee88c2f70846
Chameleonfire <ghiringhelli.pierreadrien@gmail.com>
2016-06-08 13:42:49 +0200

    userctrl

---
+++ app/Http/Middleware/Acl.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Session;
+
+
+class Acl
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @param  string|null  $guard
+     * @return mixed
+     */
+    public function handle($request, Closure $next, $guard = null)
+    {
+       $userId = Session::get('id');
+       if (!isset($userId)) {
+           return response('Unauthorised', 403);
+       }
+       return $next($request);
+    }
+}
