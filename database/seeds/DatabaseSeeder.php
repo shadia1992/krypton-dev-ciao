b60b0054af1269d82ddfc2d978e15c182627915c
randsom <sandoz.romain@gmail.com>
2016-06-08 14:15:20 +0200

    lol2

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -33,15 +33,3 @@ class DatabaseSeeder extends Seeder {
 		$this->command->info('Ressource table seeded!');
 	}
 }
-class DatabaseSeeder extends Seeder
-{
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
-    public function run()
-    {
-        // $this->call(UsersTableSeeder::class);
-    }
-}
