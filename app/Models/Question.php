547c1b950d1f5e16fda87ebb9aca6c9459bf04c0
randsom <sandoz.romain@gmail.com>
2016-06-09 12:10:42 +0200

    QRSeeds

--- app/Models/Question.php
+++ app/Models/Question.php
@@ -3,14 +3,11 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\SoftDeletingTrait;
 
 class Question extends Model {
 
 	protected $table = 'questions';
 	public $timestamps = true;
-
-	use SoftDeletingTrait;
 
 	protected $dates = ['deleted_at'];
 
