8306ee4486477ca2af63c8ed15529e1d1b4e5daa
randsom <sandoz.romain@gmail.com>
2016-06-10 16:34:05 +0200

    Update QController

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -26,7 +26,7 @@ class QuestionController extends BaseController {
    */
   public function create()
   {
-    return view('question/create');
+    return view('question/create', "caca");
   }
 
   /**
